e503f1f2932f58560791d1b80fbdf3ddc6092750
mathieu.brunot <mb.mathieu.brunot@gmail.com>
2019-04-04 21:31:23 +0200

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+
+[uri_license]: http://www.gnu.org/licenses/gpl.html
+[uri_license_image]: https://img.shields.io/badge/License-GPL%20v3-blue.svg
+
+[![License: GPL v3][uri_license_image]][uri_license]
+[![Build Status](https://travis-ci.org/Monogramm/docker-mautic.svg)](https://travis-ci.org/Monogramm/docker-mautic)
+[![Docker Automated buid](https://img.shields.io/docker/cloud/build/monogramm/docker-mautic.svg)](https://hub.docker.com/r/monogramm/docker-mautic/)
+[![Docker Pulls](https://img.shields.io/docker/pulls/monogramm/docker-mautic.svg)](https://hub.docker.com/r/monogramm/docker-mautic/)
+[![](https://images.microbadger.com/badges/version/monogramm/docker-mautic.svg)](https://microbadger.com/images/monogramm/docker-mautic)
+[![](https://images.microbadger.com/badges/image/monogramm/docker-mautic.svg)](https://microbadger.com/images/monogramm/docker-mautic)
+
+# Mautic custom Docker
+
+Custom Docker image for Mautic.
+
+Provides a Mautic with additional extensions for integrations of additional plugins.
+
+## What is Mautic ?
+
+Mautic is an Open Source Marketing Automation solution.
+
+> [mautic.org](https://mautic.org/)
+
+## Supported tags
+
+https://hub.docker.com/r/monogramm/docker-mautic/
+
+-	`2.15.1-apache`, `2.15-apache`, `production-apache`, `2.15.1`, `2.15`, `production` (*images/2.15.1-apache/Dockerfile*)
+-	`2.15.1-fpm`, `2.15-fpm`, `production-fpm` (*images/2.15.1-fpm/Dockerfile*)
+-	`2.15.1-alpine`, `2.15-alpine`, `production-alpine` (*images/2.15.1-alpine/Dockerfile*)
+
+## How to run this image ?
+
+See Mautic base image documentation for details.
+
+> [Mautic GitHub](https://github.com/Monogramm/docker-mautic-base)
+> [Mautic DockerHub](https://hub.docker.com/r/monogramm/docker-mautic-base/)
+
+# Questions / Issues
+If you got any questions or problems using the image, please visit our [Github Repository](https://github.com/Monogramm/docker-mautic) and write an issue.
---
+++ update.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+set -eo pipefail
+
+declare -A base=(
+	[apache]='debian'
+	[fpm]='debian'
+	[alpine]='alpine'
+)
+
+variants=(
+	apache
+	fpm
+)
+
+min_version='2.15'
+
+
+# version_greater_or_equal A B returns whether A >= B
+function version_greater_or_equal() {
+	[[ "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1" || "$1" == "$2" ]];
+}
+
+dockerRepo="monogramm/docker-roundcube"
+# Retrieve automatically the latest versions
+latests=( $( curl -fsSL 'https://api.github.com/repos/mautic/mautic/tags' |tac|tac| \
+	grep -oE '[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+' | \
+	sort -urV | \
+	head -n 1 )
+)
+
+# Remove existing images
+echo "reset docker images"
+find ./images -maxdepth 1 -type d -regextype sed -regex '\./images/[[:digit:]]\+\.[[:digit:]]\+' -exec rm -r '{}' \;
+
+echo "update docker images"
+travisEnv=
+for latest in "${latests[@]}"; do
+	version=$(echo "$latest" | cut -d. -f1-2)
+
+	if [ -d "$version" ]; then
+		continue
+	fi
+
+	# Only add versions >= "$min_version"
+	if version_greater_or_equal "$version" "$min_version"; then
+
+		for variant in "${variants[@]}"; do
+			echo "updating $latest [$version-$variant]"
+
+			# Create the version directory with a Dockerfile.
+			dir="images/$version-$variant"
+			mkdir -p "$dir"
+
+			template="Dockerfile-${base[$variant]}.template"
+			cp "$template" "$dir/Dockerfile"
+
+			# Replace the variables.
+			sed -ri -e '
+				s/%%VARIANT%%/-'"$variant"'/g;
+				s/%%VERSION%%/'"$latest"'/g;
+			' "$dir/Dockerfile"
+
+			travisEnv='\n    - VERSION='"$version"' VARIANT='"$variant$travisEnv"
+
+			if [[ $1 == 'build' ]]; then
+				tag="$version-$variant"
+				echo "Build Dockerfile for ${tag}"
+				docker build -t ${dockerRepo}:${tag} $dir
+			fi
+		done
+	fi
+
+done
+
+# update .travis.yml
+travis="$(awk -v 'RS=\n\n' '$1 == "env:" && $2 == "#" && $3 == "Environments" { $0 = "env: # Environments'"$travisEnv"'" } { printf "%s%s", $0, RS }' .travis.yml)"
+echo "$travis" > .travis.yml
